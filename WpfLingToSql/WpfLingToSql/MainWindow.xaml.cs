2fc509cd93da2fbf7819f7e893b091172c2b68f5
Vlad <vlad.lychagin@gmail.com>
2017-06-20 15:52:51 +0300

    Еще одна версия

--- WpfLingToSql/WpfLingToSql/MainWindow.xaml.cs
+++ WpfLingToSql/WpfLingToSql/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace WpfLingToSql
 {
     /// <summary>
-    /// главное окно приложения
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
